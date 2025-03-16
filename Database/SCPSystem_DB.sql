fb4810b95dc339f3c8796a82a2490fff62f4f71f
--unset <--unset>
2025-03-16 18:37:57 +0700

    update: implement more tables in db

--- Database/SCPSystem_DB.sql
+++ Database/SCPSystem_DB.sql
@@ -6,29 +6,47 @@ GO
 
 CREATE TABLE Brand (
   BrandId     int IDENTITY NOT NULL, 
-  Name        nvarchar(100) NOT NULL, 
+  [Name]      nvarchar(100) NOT NULL, 
   IsAvailable bit NOT NULL, 
   Username    varchar(50) NOT NULL, 
   PRIMARY KEY (BrandId));
 
 CREATE TABLE Category (
   CategoryId  int IDENTITY NOT NULL, 
-  Name        nvarchar(50) NOT NULL, 
+  [Name]      nvarchar(50) NOT NULL, 
   Quantity    int NOT NULL, 
   IsAvailable bit NOT NULL, 
   Username    varchar(50) NOT NULL, 
   PRIMARY KEY (CategoryId));
 
-CREATE TABLE Role (
+CREATE TABLE [Order] (
+  OrderId     int IDENTITY NOT NULL, 
+  DateCreated date NOT NULL, 
+  Username    varchar(50) NOT NULL, 
+  PRIMARY KEY (OrderId));
+
+CREATE TABLE OrderDetails (
+  OrderId           int NOT NULL, 
+  SkincareProductId int NOT NULL, 
+  PRIMARY KEY (OrderId, SkincareProductId));
+
+CREATE TABLE Question (
+  QuestionId int IDENTITY NOT NULL, 
+  Title      nvarchar(255) NOT NULL, 
+  Answer     nvarchar(255) NOT NULL, 
+  [Type]     varchar(255) NOT NULL, 
+  PRIMARY KEY (QuestionId));
+
+CREATE TABLE [Role] (
   RoleId int IDENTITY NOT NULL, 
-  Name   varchar(30) NOT NULL, 
+  [Name] varchar(30) NOT NULL, 
   PRIMARY KEY (RoleId));
 
 CREATE TABLE SkincareProduct (
   SkincareProductId int IDENTITY NOT NULL, 
-  Name              nvarchar(255) NOT NULL, 
-  Description       nvarchar(255) NOT NULL, 
-  Capacity          varchar(50) NOT NULL, 
+  [Name]            nvarchar(150) NOT NULL, 
+  [Description]     nvarchar(255) NULL, 
+  Capacity          int NULL, 
   UnitPrice         decimal(9, 2) NOT NULL, 
   IsAvailable       bit NOT NULL, 
   CategoryId        int NOT NULL, 
@@ -36,18 +54,28 @@ CREATE TABLE SkincareProduct (
   Username          varchar(50) NOT NULL, 
   PRIMARY KEY (SkincareProductId));
 
+CREATE TABLE TypeOfSkin (
+  TypeOfSkinId int IDENTITY NOT NULL, 
+  [Name]       nvarchar(255) NOT NULL, 
+  PRIMARY KEY (TypeOfSkinId));
+
 CREATE TABLE [User] (
-  Username varchar(50) NOT NULL, 
-  Password varchar(50) NOT NULL, 
-  Fullname nvarchar(100) NOT NULL, 
-  Gender   nvarchar(10) NOT NULL,
-  IsActive bit NOT NULL, 
-  RoleId   int NOT NULL, 
+  Username     varchar(50) NOT NULL, 
+  [Password]   varchar(50) NOT NULL, 
+  Fullname     nvarchar(100) NOT NULL, 
+  Gender	   nvarchar(5) NOT NULL,
+  IsActive     bit NOT NULL, 
+  RoleId       int NOT NULL, 
+  TypeOfSkinId int NOT NULL, 
   PRIMARY KEY (Username));
 
 ALTER TABLE SkincareProduct ADD CONSTRAINT FKSkincarePr66470 FOREIGN KEY (CategoryId) REFERENCES Category (CategoryId);
 ALTER TABLE SkincareProduct ADD CONSTRAINT FKSkincarePr37782 FOREIGN KEY (BrandId) REFERENCES Brand (BrandId);
-ALTER TABLE [User] ADD CONSTRAINT FKUser68428 FOREIGN KEY (RoleId) REFERENCES Role (RoleId);
+ALTER TABLE [User] ADD CONSTRAINT FKUser68428 FOREIGN KEY (RoleId) REFERENCES [Role] (RoleId);
 ALTER TABLE Brand ADD CONSTRAINT FKBrand47697 FOREIGN KEY (Username) REFERENCES [User] (Username);
 ALTER TABLE Category ADD CONSTRAINT FKCategory337703 FOREIGN KEY (Username) REFERENCES [User] (Username);
 ALTER TABLE SkincareProduct ADD CONSTRAINT FKSkincarePr229752 FOREIGN KEY (Username) REFERENCES [User] (Username);
+ALTER TABLE [Order] ADD CONSTRAINT FKOrder39294 FOREIGN KEY (Username) REFERENCES [User] (Username);
+ALTER TABLE OrderDetails ADD CONSTRAINT FKOrderDetai351167 FOREIGN KEY (OrderId) REFERENCES [Order] (OrderId);
+ALTER TABLE OrderDetails ADD CONSTRAINT FKOrderDetai412706 FOREIGN KEY (SkincareProductId) REFERENCES SkincareProduct (SkincareProductId);
+ALTER TABLE [User] ADD CONSTRAINT FKUser447311 FOREIGN KEY (TypeOfSkinId) REFERENCES TypeOfSkin (TypeOfSkinId);
--- Database/SCPSystem_Data.sql
+++ Database/SCPSystem_Data.sql
@@ -1,27 +1,27 @@
-﻿INSERT INTO Role(Name) values
-('Administrator'),
+﻿INSERT INTO [Role]([Name]) values
+('Manager'),
 ('Staff'),
 ('Customer');
 
-INSERT INTO [User](Username, Password, Fullname, Gender, IsActive, RoleId) values
+INSERT INTO [User](Username, [Password], Fullname, Gender, IsActive, RoleId) values
 ('admin@gmail.com', '@123', N'Nguyễn Phi Nhạn', N'Nam', 1, 1),
 ('staff@gmail.com', '@123', N'Trần Phi Mai', N'Nữ', 1, 2),
 ('customer1@gmail.com', '@123', N'Phạm Nhật Ánh', N'Nữ', 1, 3),
 ('customer2@gmail.com', '@123', N'Lu Tử Kiệt', N'Nam', 0, 3),
 ('customer3@gmail.com', '@123', N'Trần Khắc Trung', N'Nam', 1, 3);
 
-INSERT INTO Brand(Name, IsAvailable, Username) values
+INSERT INTO Brand([Name], IsAvailable, Username) values
 ('Cocoon', 1, 'admin@gmail.com'),
 ('LOreal', 1, 'admin@gmail.com'),
 ('3CE', 1, 'staff@gmail.com');
 
-INSERT INTO Category(Name, Quantity, IsAvailable, Username) values
+INSERT INTO Category([Name], Quantity, IsAvailable, Username) values
 (N'Sữa rửa mặt', 0, 1, 'staff@gmail.com'),
 (N'Mặt nạ', 0, 1, 'staff@gmail.com'),
 (N'Nước tẩy trang', 3, 1, 'staff@gmail.com'),
 (N'Son môi', 2, 1, 'staff@gmail.com');
 
-INSERT INTO SkincareProduct(Name, Description, Capacity, UnitPrice, IsAvailable, CategoryId, BrandId, Username) values
+INSERT INTO SkincareProduct([Name], [Description], Capacity, UnitPrice, IsAvailable, CategoryId, BrandId, Username) values
 (N'Micellar Water 3-in-1', N'Nước Tẩy Trang LOreal Làm Sạch Sâu Trang Điểm 400ml', '400ml', 152000, 1, 3, 2, 'staff@gmail.com'),
 (N'Nước Tẩy Trang Bí Đao', N'Cocoon Nước Tẩy Trang Bí Đao 500ml', '500ml', 140000, 1, 3, 1, 'staff@gmail.com'),
 (N'Son Kem Lì 3CE Laydown', N'Son Kem Lì 3CE Laydown - Hồng Đào Trầm Pha Nâu 4.6g Blur Water Tint', '4.6g', 277000, 1, 4, 3, 'staff@gmail.com'),
